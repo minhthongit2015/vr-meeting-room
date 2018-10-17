64d937a4da9b9410bfa4758d38b07a4a2f0df123
KID <minhthong.it2015@gmail.com>
2018-10-17 17:53:32 +0700

    test 01

--- index.js
+++ index.js
@@ -3,20 +3,37 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+let members = ["hello", "hallo", "ola"];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('a user connected: ', socket);
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('disconnect', function(info) {
+    console.log('user disconnected: ', info);
   });
 
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     console.log('message: ' + msg);
+  });
+
+
+  /// --------------------------------------------------
+
+  
+  socket.on('join-in', function(msg) {
+    io.emit('member-in', msg);
+    socket.emit("sync-in", "clients: ", JSON.stringify(members));
+    console.log('member-in: ' + msg);
+  });
+
+  socket.on('join-out', function(msg){
+    io.emit('member-out', msg);
+    console.log('member-out: ' + msg);
   });
 });
 
